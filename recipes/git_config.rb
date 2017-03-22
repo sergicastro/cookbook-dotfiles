878286bcd8dba4c4a300792be7d3d4dfc6e78231
Sergi Castro <sergikstro@gmail.com>
2017-03-22 17:51:49 +0100

    use link instead of bash

--- recipes/git_config.rb
+++ recipes/git_config.rb
@@ -1,7 +1,12 @@
-bash 'copy git config' do
-  code <<-EOF
-    set -e
-    ln -sf $dotfiles_path/git/.gitconfig $HOME/.gitconfig
-    ln -sf $dotfiles_path/git/templatedir/ $HOME/.gittemplate
-  EOF
+dotfiles_path = ENV['dotfiles_path']
+home = ENV['HOME']
+
+link home + '/.gitconfig' do
+  to dotfiles_path + '/git/.gitconfig'
+  link_type :symbolic
+end
+
+link home + '/.gittemplate' do
+  to dotfiles_path + '/git/templatedir/'
+  link_type :symbolic
 end
--- test/integration/default/serverspec/dotfiles_spec.rb
+++ test/integration/default/serverspec/dotfiles_spec.rb
@@ -12,7 +12,9 @@ end
 
 describe 'git configured' do
   it 'git config files links created' do
-    expect(file('/home/vagrant/.gitconfig')).to exist
-    expect(file('/home/vagrant/.gittemplate')).to exist
+    expect(file('/home/vagrant/.gitconfig')).to \
+      be_linked_to('/home/vagrant/.dotfiles/git/.gitconfig')
+    expect(file('/home/vagrant/.gittemplate')).to \
+      be_linked_to('/home/vagrant/.dotfiles/git/templatedir/')
   end
 end
